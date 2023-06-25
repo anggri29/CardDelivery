42035c5d7f2c51185cd0d0f0e615533cf4d3c924
Angelina <angry.anggri@gmail.com>
2023-06-25 18:03:30 +0300

    commit

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -1,12 +1,18 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.open;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CardDeliveryTest {
     @BeforeEach
     void setUp() {
@@ -19,15 +25,23 @@ public class CardDeliveryTest {
         open("http://localhost:9999/");
     }
 
+    private String getFutureDate(int daysToAdd) {
+        LocalDate currentDate = LocalDate.now();
+        LocalDate futureDate = currentDate.plusDays(daysToAdd);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String formattedDate = futureDate.format(formatter);
+        return formattedDate;
+    }
+
     @Test
     public void shouldTest() throws InterruptedException {
-        $("[data-test-id=name] input").sendKeys("Григорян Ангелина");
-        $("[data-test-id=phone] input").sendKeys("+79250881558");
+        $("[data-test-id=city] input").setValue("Москва");
+        $("[data-test-id=date] input").setValue(getFutureDate(3));
+        $("[data-test-id=name] input").setValue("Григорян Ангелина");
+        $("[data-test-id=phone] input").setValue("+79250881558");
         $("[data-test-id=agreement]").click();
         $("button").click();
-        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-        String actual = $("[data-test-id=order-success]").getText().trim();
-        assertEquals(expected, actual);
-        Thread.sleep(5000);
+
+        $(withText(" Успешно!")).shouldBe(Condition.hidden, Duration.ofSeconds(15));
     }
 }
